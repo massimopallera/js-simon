028f2ed77553b8a028a00e05e61e63ada1249f4f
massimopallera <pallera.massimo@gmail.com>
2024-10-10 15:29:59 +0200

    add: random numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 
+const n = document.getElementById('numbers')
+
+
 //create 5 random numbers
-let randoms = (maxCont=5) => {
+const randoms = (maxCont=5) => {
   let arr = []
   for (let i = 0; i < maxCont; i++){
     const n = Math.floor(Math.random() * 10) + 1
@@ -9,3 +12,16 @@ let randoms = (maxCont=5) => {
   return arr
 }
 
+n.innerHTML = printRandoms(randoms())
+
+//prints the random numbers array
+function printRandoms(arr){
+  let markup = ``
+  for (let i = 0; i < arr.length; i++){
+    markup += 
+    `
+      <div>${arr[i]}</div>
+    `
+  }
+  return markup
+}
